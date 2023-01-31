03652448013207167595535ac230c73299f492af
Wilson Wu <wuyecheng738@gmail.com>
2023-01-30 22:23:46 -0500

    end of day push

--- pokedex-rails-backend/config/routes.rb
+++ pokedex-rails-backend/config/routes.rb
@@ -3,4 +3,14 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get "/api/pokemon/types", to: "api#pokemon#types"
+
+  namespace :api, defaults: { format: :json } do
+    resources :pokemon, only: [ :index, :show, :create, :update] do
+      resources :items, only: [ :index, :create]
+    end
+    resources :items, only: [ :update, :destroy]
+    
+  end
+  # get “/api/pokemon/types”, to: “api#pokemon#types”
 end
